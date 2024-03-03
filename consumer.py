b314e6e81e90e464259fe7950acf0d4a78bc3f93
VolodymyrRiz <v.v.rizun@gmail.com>
2024-03-03 23:31:42 +0200

    ht8-8

--- consumer.py
+++ consumer.py
@@ -5,26 +5,34 @@ import pika
 import time
 import json
 
-credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
-channel = connection.channel()
+def main():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+    channel = connection.channel()
 
-channel.queue_declare(queue='hello_world', durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
-
-
-def callback(ch, method, properties, body):
-    message = json.loads(body.decode())
-    print(f" [x] Received {message}")
-    time.sleep(1)
-    print(f" [x] Done: {method.delivery_tag}")
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    channel.queue_declare(queue='hello_world')
 
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='hello_world', on_message_callback=callback)
+
+    def callback(ch, method, properties, body):    
+        print(f" [x] Received {body.decode()}")
+        # time.sleep(1)
+        # print(f" [x] Done: {method.delivery_tag}")
+        # ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    #channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='hello_world', on_message_callback=callback, auto_ack=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 
 if __name__ == '__main__':
-    channel.start_consuming()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- producer.py
+++ producer.py
@@ -1,17 +1,25 @@
+import json
+from datetime import datetime
 import pika
 
 
-def main():
-    credentials = pika.PlainCredentials('guest', 'guest')
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
-    channel = connection.channel()
-    
-    channel.queue_declare(queue='hello_world')
-    
-    channel.basic_publish(exchange='', routing_key='hello_world', body='Hello world!'.encode())
-    print(" [x] Sent 'Hello World!'")
-    connection.close()
-    
+credentials = pika.PlainCredentials('guest', 'guest')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+channel = connection.channel()
 
+channel.exchange_declare(exchange='WEB_RIZ', exchange_type='direct')
+channel.queue_declare(queue='web_riz_queue', durable=True)
+channel.queue_bind(exchange='WEB_RIZ', queue='web_riz_queue')
+    
+def create_task(nums: int):
+    for i in range(nums):
+        message = {
+            'id': i,
+            'payload': f'Date: {datetime.now().isoformat}'
+        }
+    
+        channel.basic_publish(exchange='WEB_RIZ', routing_key='web_riz_queue', body=json.dumps(message).encode())
+    connection.close()
+       
 if __name__ == '__main__':
-    main()
+    create_task(100)
