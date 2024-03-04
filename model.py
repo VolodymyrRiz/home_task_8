61cce9613ffc6c85b893f96e1820cd6ab8a12af0
VolodymyrRiz <v.v.rizun@gmail.com>
2024-03-04 10:39:57 +0200

    ht8-9

--- model.py
+++ model.py
@@ -1,15 +1,9 @@
 from datetime import datetime
 
-from mongoengine import EmbeddedDocument, Document
+from mongoengine import *
 from mongoengine.fields import ObjectId, ReferenceField, BooleanField, DateTimeField, EmbeddedDocumentField, ListField, StringField
 
-
-class Contact(Document):    
-    mail = StringField()
-   
-    description = StringField()
-    
-class quotes(Document):
-    author_id = ReferenceField(nnij)
-    quote = StringField()
-    tags = ListField()
+class Contact(Document):  
+    fullname = StringField(max_length=150)
+    mail = StringField(max_length=150)
+    log_field = BooleanField(default=False)
